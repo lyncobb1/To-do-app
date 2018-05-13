00128de15d23abaa1ec80d1a7558ba15969387a7
Timberlin Cobb <tmcobb009@hotmail.com>
2018-05-13 15:41:37 -0700

    Corrections to HW

---
+++ To-do-app/app.js
@@ -0,0 +1,42 @@
+function onReady () {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+
+  const deleteButton = document.getElementById('deleteButton');
+  // add event listenr for delete button click
+  // // find all checked checkboxes
+  // // remove those from the toDoList
+
+   addToDoForm.addEventListener('submit', event => {
+     event.preventDefault();
+     let title = newToDoText.value;
+     let newLi = document.createElement('li');
+     let checkbox = document.createElement('input');
+     checkbox.type = "checkbox";
+     newLi.textContent = title;
+     newLi.appendChild(checkbox);
+     toDoList.appendChild(newLi);
+     newToDoText.value = '';
+   });
+
+}
+
+  deleteButton.addEventListener('click', event => {
+     event.preventDefault();
+     const box = document.getElementById('myCheckBox');
+     const text = document.getElementById('newLi');
+
+       if (box.checked) {
+         box.parentNode.removeChild(myCheckBox);
+         text.parentNode.removeChild(newLi);
+     }
+
+   });
+
+
+
+window.onload = function (){
+  onReady();
+};
