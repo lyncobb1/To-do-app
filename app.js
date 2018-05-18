8e447eb7d01335971c29f1622c95915a600e1d2e
Timberlin Cobb <tmcobb009@hotmail.com>
2018-05-17 18:12:57 -0700

    Completed HW_C11

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 function onReady () {
+
+  let toDos =[];
+
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-  const deleteButton = document.getElementById('deleteButton');
-  // add event listenr for delete button click
-  // // find all checked checkboxes
-  // // remove those from the toDoList
 
-   addToDoForm.addEventListener('submit', event => {
-     event.preventDefault();
-     let title = newToDoText.value;
-     let newLi = document.createElement('li');
-     let checkbox = document.createElement('input');
-     checkbox.type = "checkbox";
-     newLi.textContent = title;
-     newLi.appendChild(checkbox);
-     toDoList.appendChild(newLi);
-     newToDoText.value = '';
+function createNewToDo () {
+  if(!newToDoText.value) { return; }
+
+  toDos.push({
+    title: newToDoText.value,
+    complete: false
    });
 
-   deleteButton.addEventListener('click', event => {
-     event.preventDefault();
-     const box = document.getElementById('myCheckBox').checked;
-     const text = document.getElementById('newLi');
-     if (box.checked) {
-         box.parentNode.removeChild(myCheckBox);
-         text.parentNode.removeChild(newLi);
-      }
-   });
-
+   newToDoText.value ='';
+   renderTheUI();
   }
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+    newToDoText.value = '';
+  });
+
+function renderTheUI() {
+  const toDoList = document.getElementById('toDoList');
+
+  toDoList.textContent = '';
+
+  toDos.forEach(function(toDo) {
+    const newToDo = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    newToDo.textContent = toDo.title;
+    toDoList.appendChild(newToDo);
+    newToDo.appendChild(checkbox);
+  });
+}
+
+  renderTheUI();
+ }
 
 window.onload = function (){
   onReady();
